899e854e23ed6ae6c900c00c06c44ab739106b25
Mohammad Issawi <2000issawi@gmail.com>
2023-12-08 01:59:34 +0300

    Implement Grid::get_window

--- grid_puzzle/grid.py
+++ grid_puzzle/grid.py
@@ -1,3 +1,5 @@
+import cv2
+
 # Sizes are defined (width,height)
 class Grid:
     def __init__(self, img_colored, size):
@@ -9,3 +11,14 @@ class Grid:
         c = coordinates
         return self.img[c[1] * self.piece_size[1]:c[1] * self.piece_size[1] + self.piece_size[1],
                c[0] * self.piece_size[0]:c[0] * self.piece_size[0] + self.piece_size[0]]
+    def get_window(self,two_pieces,vertical=True,window_ratio=0.1):
+        if vertical:
+            concated_pieces=cv2.vconcat(two_pieces)
+            concated_pieces_center_vertically=two_pieces[0].shape[0]
+            window_size=int(two_pieces[0].shape[0]*window_ratio)
+            return concated_pieces[concated_pieces_center_vertically-window_size:concated_pieces_center_vertically+window_size,:]
+        else:
+            concated_pieces=cv2.hconcat(two_pieces)
+            concated_pieces_center_horizontally=two_pieces[0].shape[1]
+            window_size=int(two_pieces[0].shape[1]*window_ratio)
+            return concated_pieces[:,concated_pieces_center_horizontally-window_size:concated_pieces_center_horizontally+window_size]
