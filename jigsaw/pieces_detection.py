8afd22abb036c665dabc06291640fc1023163c3e
Bshr Al Homsi <bishosyr.co@gmail.com>
2023-12-08 13:44:47 +0300

    improving pieces_detection

--- jigsaw/pieces_detection.py
+++ jigsaw/pieces_detection.py
@@ -9,6 +9,8 @@ def extract_pieces(img):
     img_blur=cv2.GaussianBlur(img,(5,5),None)
     img_gray=cv2.cvtColor(img_blur,cv2.COLOR_BGR2GRAY)
     _,img_bin=cv2.threshold(img_gray,20,255,cv2.THRESH_BINARY)
+    kernel_closing = np.ones((9, 9), np.uint8)
+    img_bin = cv2.morphologyEx(img_bin, cv2.MORPH_CLOSE, kernel_closing)
     img_bin=cv2.GaussianBlur(img_bin,(5,5),None)
     contours,hiarachy=cv2.findContours(img_bin,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     masks=[]
@@ -16,10 +18,12 @@ def extract_pieces(img):
         mask=np.zeros(np.shape(img_bin),'uint8')
         mask=cv2.drawContours(mask,[contour],-1,255,cv2.FILLED)
         masks.append(mask)
-    pieces=[]
-    for mask,contour in zip(masks,contours):
-        piece=cv2.bitwise_and(img,img,mask=mask)
-        x,y,w,h=cv2.boundingRect(contour)
-        piece=piece[y:y+h,x:x+w]
-        pieces.append(Piece(x,y,w,h,piece,mask,contour,PieceType.UNKNOWN))
+    min_piece_area = 5000 
+    pieces = []
+    for mask, contour in zip(masks, contours):
+        x, y, w, h = cv2.boundingRect(contour)
+        if w * h > min_piece_area:
+            piece = cv2.bitwise_and(img, img, mask=mask)
+            piece = piece[y:y + h, x:x + w]
+            pieces.append(Piece(x, y, w, h, piece, mask, contour, PieceType.UNKNOWN))   
     return pieces
