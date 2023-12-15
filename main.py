e93e3ce5d4a73ef5b543f0deda4749a2614bf636
Maias <maias-alakramy01@hotmail.com>
2023-12-15 10:08:40 +0300

    name windows

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ img = img_utils.read_img('./samples/christmas-cats-500x204.jpg')
 
 grid = Grid(img, (2,1), shuffle=True, hint=img)
 
-img_utils.display_img(grid.get_pieces_img())
+img_utils.display_img(grid.get_pieces_img(),"Puzzle")
 
 grid.process_all_pieces()
 grid.clean_up_dicts()
 
 solver = GreedySolver(grid)
 solutions = solver.solve()
-img_utils.display_img(solver.get_solution_img(solutions[0]))
+img_utils.display_img(solver.get_solution_img(solutions[0]),"Solution")
