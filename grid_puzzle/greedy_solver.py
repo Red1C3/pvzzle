790c07339045a02567b754bdba3cf1230d3cf465
Maias <maias-alakramy01@hotmail.com>
2023-12-15 10:04:11 +0300

    add score to the solutions

--- grid_puzzle/greedy_solver.py
+++ grid_puzzle/greedy_solver.py
@@ -8,9 +8,9 @@ class GreedySolver:
         self.solutions = []
         self.grid = grid
 
-    def _solve(self, pieces, current_loc):
+    def _solve(self, pieces, current_loc,score):
         if len(pieces) == len(self.grid.pieces):
-            self.solutions.append(pieces)
+            self.solutions.append((pieces,score))
             return
         if current_loc[0] != 0:
             solve_dict = pieces[current_loc].left_dict
@@ -18,7 +18,7 @@ class GreedySolver:
                 if p not in pieces.values():
                     pieces_cpy = pieces.copy()
                     pieces_cpy[(current_loc[0] - 1, current_loc[1])] = p
-                    self._solve(pieces_cpy, (current_loc[0] - 1, current_loc[1]))
+                    self._solve(pieces_cpy, (current_loc[0] - 1, current_loc[1]),score+solve_dict[p])
         else:
             current_loc = (self.grid.size[0] - 1, current_loc[1])
             solve_dict = pieces[current_loc].up_dict
@@ -26,20 +26,20 @@ class GreedySolver:
                 if p not in pieces.values():
                     pieces_cpy = pieces.copy()
                     pieces_cpy[(current_loc[0], current_loc[1] - 1)] = p
-                    self._solve(pieces_cpy, (current_loc[0], current_loc[1] - 1))
+                    self._solve(pieces_cpy, (current_loc[0], current_loc[1] - 1),score+solve_dict[p])
 
     def solve(self):
         self.solutions = []
         grid_size = self.grid.size
         for piece in self.grid.pieces:
-            self._solve({(grid_size[0] - 1, grid_size[1] - 1): piece}, (grid_size[0] - 1, grid_size[1] - 1))
+            self._solve({(grid_size[0] - 1, grid_size[1] - 1): piece}, (grid_size[0] - 1, grid_size[1] - 1),0)
         return self.solutions
 
     def get_solution_img(self, solution):
         grid_size = self.grid.size
         piece_size = self.grid.piece_size
         s_img = np.zeros((grid_size[1] * piece_size[1], grid_size[0] * piece_size[0], 3), 'uint8')
-        for loc, piece in solution.items():
+        for loc, piece in solution[:][0].items():
             s_img[loc[1] * piece_size[1]:loc[1] * piece_size[1] + piece_size[1],
             loc[0] * piece_size[0]:loc[0] * piece_size[0] + piece_size[0]] = piece.img
 
