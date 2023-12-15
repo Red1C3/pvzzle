341e9bd1f6d91ba269190b0b422393aff9b75bcd
Bshr Al Homsi <bishosyr.co@gmail.com>
2023-12-15 20:44:05 +0300

    top contour problem solved

--- jigsaw/pieces_detection.py
+++ jigsaw/pieces_detection.py
@@ -62,56 +62,22 @@ def contour_splitter(piece):
         closest_point = tuple(contour_points[closest_index])
         closest_points.append(closest_point)
         Indexs.append(closest_index)
-        '''
-        ## code for debugging porposes only
-        num_of_left=Indexs[1]-Indexs[0]
-        num_of_bottom=Indexs[2]-Indexs[1]
-        num_of_right=Indexs[3]-Indexs[2]
-        num_of_top=len_contour_points-Indexs[3]
-        print(num_of_left,num_of_bottom,num_of_right,num_of_top)
-        '''
-
     # handle every status for edges
     left_side = contour_points [Indexs[0]:Indexs[1]+1]
     bottom_side = contour_points [Indexs[1]:Indexs[2]+1]
     right_side= contour_points [Indexs[2]:Indexs[3]+1]
     t1=contour_points [:Indexs[0]+1]
     t2=contour_points [Indexs[3]:len_contour_points+1]
-    top_side.extend(t1)
-    top_side.extend(t2) 
-    # Assignment of edge arrays
+    t1=t1[::-1]
+    t2=t2[::-1]
+    top_side = np.concatenate((t1, t2))
     piece.left_contour=left_side
     piece.bottom_contour=bottom_side
     piece.right_contour=right_side[::-1]
     piece.top_contour=top_side
     piece.corners=closest_points
-   
-    '''
-                 ##  VISUALISATION  ##
-    
-    # Draw the points on the piece image
-    piece_img_with_points = piece.sub_img.copy()
-    #  1     points: left_up -> left_down 
-    for point in left_side:
-        cv2.circle(piece_img_with_points, point, 5, (0, 100, 100), -1)  
-    #   2    points: left_down -> right_down 
-    for point in bottom_side:
-        cv2.circle(piece_img_with_points, point, 5, (100, 100, 0), -1)  
-    #   3    points: right_down -> right_up
-    for point in right_side:
-        cv2.circle(piece_img_with_points, point, 5, (100, 0, 100), -1)  
-    #   4    points: right_up -> left_up  
-    for point in top_side:
-        cv2.circle(piece_img_with_points, point, 5, (100, 100, 100), -1)  
-    for point in closest_points:
-        cv2.circle(piece_img_with_points, point, 5, (0, 0, 255), -1)   
-    # Display the piece image with points
-    cv2.imshow('Piece Image with Points', piece_img_with_points)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-          
 
-    '''
+
             ### EXTRACT PIECES WITH ALL FEAUTERS SET ###
 
 
