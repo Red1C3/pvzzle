2cb0f8fc9b159ef93b89e7a2e0b579079bcc9af1
Mohammad Issawi <2000issawi@gmail.com>
2023-12-17 22:08:43 +0300

    WIP template match and bf template match

--- jigsaw/jigsaw.py
+++ jigsaw/jigsaw.py
@@ -4,6 +4,7 @@ from sklearn.cluster import KMeans
 
 from jigsaw.pieces_detection import extract_pieces
 from jigsaw.set_piece_type import set_piece_type
+from utils import img_utils
 
 
 class Jigsaw:
@@ -70,6 +71,32 @@ class Jigsaw:
             piece = piece[0]
             pimg = piece.sub_img
             pshape = pimg.shape
+            center = (center[0] * 1.5, center[1] * 1.5)
             cimg[round(center[1]-pshape[0]/2)+offset[0]:round(center[1]+pshape[0]/2)+offset[0],
                  round(center[0]-pshape[1]/2)+offset[1]:round(center[0]+pshape[1]/2)+offset[1]] += pimg
         return cimg
+
+    def template_match(self):
+        for piece in self.pieces:
+            w, h = piece.sub_img.shape[1], piece.sub_img.shape[0]
+            res = cv2.matchTemplate(self.hint, piece.sub_img, cv2.TM_CCOEFF)
+            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+            top_left = max_loc
+            bottom_right = (top_left[0] + w, top_left[1] + h)
+            cv2.rectangle(self.hint, top_left, bottom_right, 255, 2)
+            img_utils.display_img(self.hint)
+
+    def template_match2(self):
+        solution = np.zeros(self.hint.shape, 'uint8')
+        for piece in self.pieces:
+            h_shift = self.hint.shape[0] - piece.sub_img.shape[0]
+            w_shift = self.hint.shape[1] - piece.sub_img.shape[1]
+            for h in range(h_shift):
+                for w in range(w_shift):
+                    piece_img = np.zeros(self.hint.shape, 'uint8')
+                    piece_img[h:h + piece.sub_img.shape[0], w:w + piece.sub_img.shape[1]] = piece.sub_img
+                    xor_img = cv2.bitwise_xor(piece_img, self.hint)
+                    img_utils.display_img(xor_img)
+                    if not np.any(xor_img):
+                        solution += piece_img
+        return solution
--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import cv2
-import numpy as np
-
-from jigsaw import pieces_detection
 from jigsaw.jigsaw import Jigsaw
-from jigsaw.match_directions import MatchDir
-from jigsaw.pieces_types import PieceType
 from utils import img_utils
 
 img = img_utils.read_img('./samples/lenna_jigsaw.png')
@@ -12,28 +6,9 @@ hint = img_utils.read_img('./samples/lenna.png')
 
 jigsaw = Jigsaw(img, hint)
 
-clusters = jigsaw.cluster()
+# img_utils.display_img(jigsaw.clusters_img())
+img_utils.display_img(jigsaw.template_match2())
 
-for piece, center in zip(clusters[0], clusters[1]):
-    img_utils.display_img(piece[0].sub_img, str(center))
 
-pieces = pieces_detection.extract_pieces(img)
-
-pieces[0].type = PieceType.LEFT_UP
-pieces[0].sol_x = 0
-pieces[0].sol_y = 0
-match, padding = pieces[0].match(pieces[1], 100, MatchDir.RIGHT, max_error=0.1)
-pieces[1].sol_x = int(pieces[0].sol_x + padding[3] - padding[2])
-pieces[1].sol_y = int(pieces[0].sol_y + padding[0] - padding[1])
-new_img = np.zeros(img.shape, 'uint8')
-
-new_img[0:pieces[0].sub_img.shape[0],0:pieces[0].sub_img.shape[1]]+=pieces[0].sub_img
-
-print(padding)
-temp_img=cv2.copyMakeBorder(pieces[1].sub_img,padding[0],padding[1],int(padding[2]),int(padding[3]),cv2.BORDER_CONSTANT,value=(0,0,0))
-
-new_img[0:temp_img.shape[0],0:temp_img.shape[1]]+=temp_img
-
-img_utils.display_img(new_img)
 
 img_utils.close_all_windows()
