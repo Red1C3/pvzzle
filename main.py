91d1370dd41c04bab22e30e0e1a4dc10a58d9398
Mohammad Issawi <2000issawi@gmail.com>
2023-12-19 14:09:49 +0300

    Add all jigsaw with hint algorithms

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from jigsaw.jigsaw import Jigsaw
-from grid_puzzle.grid import Grid
 from grid_puzzle.greedy_solver import GreedySolver
-from grid_puzzle.hint_solver import HintSolver
+from grid_puzzle.grid import Grid
 from grid_puzzle.hint_quant_solver import HintQuantSolver
+from grid_puzzle.hint_solver import HintSolver
+from jigsaw.jigsaw import Jigsaw
 from utils import img_utils
+
 
 def grid_without_hint(size,img_path):
     img = img_utils.read_img(img_path)
 
-    grid = Grid(img, size, shuffle=True, hint=img)
+    grid = Grid(img, size, shuffle=True, hint=None)
 
     img_utils.display_img(grid.get_pieces_img(),"Puzzle")
 
@@ -39,21 +40,31 @@ def grid_with_hint(size,img_path,hint_path=None,solver_type="Q"):
     for s in solutions:
         img_utils.display_img(solver.get_solution_img(s),"Solution")
 
-def jigsaw_with_hint(img_path,hint_path):
+
+def jigsaw_with_hint(img_path, hint_path, solver_type):
     img = img_utils.read_img(img_path)
     hint = img_utils.read_img(hint_path)
 
+    img_utils.display_img(img, 'Puzzle')
+    img_utils.display_img(hint, "Hint")
+
     jigsaw = Jigsaw(img, hint)
 
-    # img_utils.display_img(jigsaw.clusters_img())
-    img_utils.display_img(jigsaw.grid_match(10))
+    match solver_type:
+        case 'clusters':
+            img_utils.display_img(jigsaw.clusters_img(), 'Solution')
+        case 'grid_quantization':
+            img_utils.display_img(jigsaw.grid_match(5), 'Solution')
+        case 'bruteforce_quantization':  # Takes a lot of time
+            img_utils.display_img(jigsaw.template_match2(), 'Solution')
 
-img_path = './samples/cat1.jpg'
+
+img_path = './samples/lenna.png'
 hint_path = './samples/lenna.png'
 
-grid_without_hint(size=(5,5),img_path=img_path)
-grid_with_hint(size=(5,5),img_path=img_path,hint_path=None,solcer_type="Q")
-# jigsaw_with_hint(img_path=img_path,hint_path=hint_path)
+# grid_without_hint(size=(5,5),img_path=img_path)
+# grid_with_hint(size=(5,5),img_path=img_path,hint_path=None,solver_type="Q")
+# jigsaw_with_hint(img_path=img_path,hint_path=hint_path,solver_type='clusters')
 
 
 img_utils.close_all_windows()
